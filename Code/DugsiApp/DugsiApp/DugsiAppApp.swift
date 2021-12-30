974b64fb5223df58acb668e4eaf9457a22c115ab
ilyasarif100 <92511650+ilyasarif100@users.noreply.github.com>
2021-12-30 00:01:46 -0700

    Added Swift code

---
+++ Code/DugsiApp/DugsiApp/DugsiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DugsiAppApp.swift
+//  DugsiApp
+//
+//  Created by Ilyas Arif on 12/30/21.
+//
+
+import SwiftUI
+
+@main
+struct DugsiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
